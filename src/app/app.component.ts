cee933cf3d8dc9738d7915c4d1470b230a4fa927
Cinthia M. Orona <devrider2021@gmail.com>
2022-01-25 08:38:24 -0300

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NoticiaService } from './services/noticia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'noticias';
-}
+  listNoticias: any[] = [];
+  loading = false;
+  
+  constructor(private noticiaService: NoticiaService){  }
+
+  buscarNoticias(parametros: any){
+    this.loading = true;
+    this.listNoticias = [];
+
+    setTimeout(() => {
+      this.noticiaService.getNoticias(parametros).subscribe(data => {
+        // console.log(data);
+        this.loading = false;
+  
+        this.listNoticias = data.articles;
+      }, error =>{
+        console.log(error);
+        this.loading = false;
+      })
+      
+    }, 1000);    
+  }
+} 
